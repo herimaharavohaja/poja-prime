8a2d270288c7fc8a2a52e8b59a70c67b847f18d4
herimaharavohaja <hei.haja.25@gmail.com>
2023-12-12 12:31:47 +0100

    first commit

---
+++ src/main/java/com/poja/base/endpoint/rest/controller/HealthController.java
@@ -0,0 +1,46 @@
+package com.poja.base.endpoint.rest.controller;
+
+import static java.util.UUID.randomUUID;
+
+import java.util.List;
+import lombok.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.poja.base.PojaGenerated;
+import com.poja.base.endpoint.event.EventProducer;
+import com.poja.base.endpoint.event.gen.UuidCreated;
+import com.poja.base.repository.DummyRepository;
+import com.poja.base.repository.DummyUuidRepository;
+import com.poja.base.repository.model.Dummy;
+import com.poja.base.repository.model.DummyUuid;
+
+@PojaGenerated
+@RestController
+@Value
+public class HealthController {
+
+  DummyRepository dummyRepository;
+  DummyUuidRepository dummyUuidRepository;
+  EventProducer eventProducer;
+
+  @GetMapping("/ping")
+  public String ping() {
+    return "pong";
+  }
+
+  @GetMapping("/dummy-table")
+  public List<Dummy> dummyTable() {
+    return dummyRepository.findAll();
+  }
+
+  @GetMapping(value = "/uuid-created")
+  public String uuidCreated() throws InterruptedException {
+    var randomUuid = randomUUID().toString();
+    var event = new UuidCreated().toBuilder().uuid(randomUuid).build();
+
+    eventProducer.accept(List.of(event));
+
+    Thread.sleep(20_000);
+    return dummyUuidRepository.findById(randomUuid).map(DummyUuid::getId).orElseThrow();
+  }
+}
